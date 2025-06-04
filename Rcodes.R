b8e3803180f145058093f03653f3120075bd5209
Wanahyunn <143162467+NurulShafwana@users.noreply.github.com>
2025-06-04 09:08:38 +0800

    rcodes and file
    removing excel data due to error.

--- Rcodes.R
+++ Rcodes.R
@@ -16,8 +16,7 @@ mydata <- read_excel("Brunei Digital Skills 2024.xlsx")
 # Check if data is correct - not necessary to do
 head(mydata)
 
-# Removing one row from the excel data
-# Remove row 1 from the data frame
+# Removing one row from the excel data frame
 mydata <- mydata[-1, ]
 
 
@@ -32,5 +31,8 @@ mydata <- mydata[-1, ]
 ### In this case, we use ids (or cluster) as HH,
 ###                       strata as Gender,
 ###                       and no weights used.
-bruneidesign_ind24 <- svydesign(ids = ~HH, strata = ~GEN, data = mydata,
+bruneidesign_ind24 <- svydesign(ids = ~HH, strata = ~AREA, data = mydata,
                                 nest = TRUE)
+
+# Save the file as Rdata file
+save(bruneidesign_ind24, file = "bruneidesign_ind24.Rdata")
