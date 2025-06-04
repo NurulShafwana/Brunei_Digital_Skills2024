31c9cf87b02ea97b8fdda69a0da9cf655bf51daf
Wanahyunn <143162467+NurulShafwana@users.noreply.github.com>
2025-06-04 08:39:33 +0800

    rcodes

---
+++ Rcodes.R
@@ -0,0 +1,36 @@
+# === These codes will turn Brunei Digital Skills 2024 excel file to Rdata === #
+# ================= To easily access this data in Rstudio ==================== #
+# ================= This only need to be run ONE TIME ======================== #
+
+# Install packages
+install.packages("readxl")    # readxl for extracting excel file
+install.packages("survey")    # survey for turning data into survey form
+
+# Load libraries to use the packages
+library(readxl)
+library(survey)
+
+# Read excel file
+mydata <- read_excel("Brunei Digital Skills 2024.xlsx")
+
+# Check if data is correct - not necessary to do
+head(mydata)
+
+# Removing one row from the excel data
+# Remove row 1 from the data frame
+mydata <- mydata[-1, ]
+
+
+# Creating survey design using the data
+## I named the survey as bruneidesign_ind24
+### | Term      | Formal Definition                          | Kids Version                                     |
+### | --------- | ------------------------------------------ | ------------------------------------------------ |
+### | `ids`     | Primary sampling unit                      | Which group you visited (e.g., classroom)        |
+### | `strata`  | Population subgroups used before sampling  | Types of groups you made (e.g., city or village) |
+### | `weights` | How many people each respondent represents | How much each kid’s answer counts                |
+
+### In this case, we use ids (or cluster) as HH,
+###                       strata as Gender,
+###                       and no weights used.
+bruneidesign_ind24 <- svydesign(ids = ~HH, strata = ~GEN, data = mydata,
+                                nest = TRUE)
